e51b3f8e61323917638f1dc8169d00878309f455
EricoBis <ericobis.santos@gmail.com>
2023-03-22 19:15:47 -0300

    add | base of the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GlobalStyle from "./styles";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <>hello world!</>
+    </div>
+  );
+}
+
+export default App;
